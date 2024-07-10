453e26fae9942d1f3ea872e3a2edcb5ce90eed97
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-07-09 19:39:20 -0700

    actually use the wrappers home position

--- modules/UR5Wrapper.py
+++ modules/UR5Wrapper.py
@@ -11,7 +11,7 @@ class UR5Wrapper:
         self.max_movement = 0.3
         self.move_time = 0.4
         self.init_pose = [1.57, -1.57, -2.66, -0.6, -1.57, 0]
-        self.home_pose = self.robot.get_cur_pose_with_rot()
+        self.home_position = self.robot.get_cur_pose_with_rot()
 
 
     def set_home_position(self):
@@ -20,8 +20,8 @@ class UR5Wrapper:
     # this will be in delta relative to home position
     def go_to_position(self, position, wait=False):
         curr_pose = self.robot.get_cur_pose_with_rot()
-        desried_pose = getMovement(curr_pose, position, self.max_movement)
-        self.robot.servojInvKin(desried_pose, wait=wait, t=self.move_time)
+        desired_pose = getMovement(curr_pose, self.home_position + position, self.max_movement)
+        self.robot.servojInvKin(desired_pose, wait=wait, t=self.move_time)
 
     def reset_to_init(self):
         self.robot.movej(self.home_position)
